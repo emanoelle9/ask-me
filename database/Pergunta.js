d627dbb72cd74597a466cd3f81ad5a79f47d587b
emanoelle9 <emanoelleferreiramateus@gmail.com>
2024-07-01 11:40:42 -0300

    Configuração do banco de dados e riação ddo model e Pergunta

---
+++ database/Pergunta.js
@@ -0,0 +1,16 @@
+const {Sequelize} = require('@sequelize/core')
+const connection = require('./database')
+
+const Pergunta = connection.define('pergunta',{
+    titulo: {type: Sequelize.STRING, allowNull: false},
+    descricao: {type: Sequelize.TEXT, allowNull: false},
+})
+
+//CREATE TABLE IF NOT EXISTS
+Pergunta.sync({force: false})
+    .then(()=>{
+        console.log('Tabela pergunta sicronizada')
+        })
+    .catch((error) =>{
+        console.log('Erro ao criar a tabela')
+})
--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const app = express()
+const bodyparser = require('body-parser')
+const connection = require('./database/database')
+const Pergunta = require('./database/Pergunta')
 const port = 3000
+
+connection
+        .authenticate()
+        .then(() => {
+            console.log("Banco de daods conectado com sucesso!")
+        }).catch((msgErro)=>{
+            console.log(msgErro)
+
+        })
 
 app.set('view engine', 'ejs')
 
