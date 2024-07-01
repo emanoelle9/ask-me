17bbd878150abc91eff82d34e0817af12c5f4000
emanoelle9 <emanoelleferreiramateus@gmail.com>
2024-07-01 08:48:25 -0300

    implementaçãoda rota de salvar a pergunta

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) =>{
+    res.render("index")
+})
+
+app.get('/perguntar', (req, res) =>{
+    res.render('perguntar')
+})
+
+app.listen(port, (erro) =>{
+    if(erro){
+        console.log("Erro ao iniciar o servidor")
+    }else{
+        console.log(`Servidor rodando em https://localhost:${port}`)
+    }
+})
