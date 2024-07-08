4cdc3c7a4b9c614ab9fe330b298d5646f13a0936
emanoelle9 <emanoelleferreiramateus@gmail.com>
2024-07-08 11:15:44 -0300

    Finalização da página de respostas

--- database/database.js
+++ database/database.js
@@ -1,23 +1,23 @@
-// const {Sequelize} = require('@sequelize/core')
-// const {MariaDbDialect} = require('@sequelize/mariadb')
-
-// const connection = new Sequelize({
-//     dialect: MariaDbDialect,
-//     database: 'askme',
-//     user: 'administrador',
-//     password: '123456',
-//     host: '172.16.96.177',
-//     port:3306,
-//     showWarnings: true,
-//     connectTimeout: 1000,
-// })
-
 const {Sequelize} = require('@sequelize/core')
-const {SqliteDialect} = require( '@sequelize/sqlite3')
+const {MariaDbDialect} = require('@sequelize/mariadb')
 
 const connection = new Sequelize({
-    dialect: SqliteDialect,
-    storage: 'database/askme.db'
+    dialect: MariaDbDialect,
+    database: 'askme',
+    user: 'administrador',
+    password: '123456',
+    host: '172.16.96.177',
+    port:3306,
+    showWarnings: true,
+    connectTimeout: 1000,
 })
+
+// const {Sequelize} = require('@sequelize/core')
+// const {SqliteDialect} = require( '@sequelize/sqlite3')
+
+// const connection = new Sequelize({
+//     dialect: SqliteDialect,
+//     storage: 'database/askme.db'
+// })
 
 module.exports = connection
